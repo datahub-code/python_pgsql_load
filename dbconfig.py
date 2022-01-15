06c21199a5297985b11e9f95464774946bd196dd
datahub-code <fahimuddinbd@gmail.com>
2022-01-15 12:41:34 -0500

    UPDATED FILE

---
+++ dbconfig.py
@@ -0,0 +1,21 @@
+import os
+
+DB_DETAILS = {
+    'dev': {
+        'SOURCE_DB': {
+            'DB_TYPE': 'postgresql',
+            'DB_HOST': 'postgresql',
+            'DB_NAME': 'postgresql',
+            'DB_USER': 'postgresql',
+            'DB_PASS': 'postgresql'
+        },
+        'TARGET_DB': {
+            'DB_TYPE': 'postgresql',
+            'DB_HOST': 'postgresql',
+            'DB_NAME': 'postgresql',
+            'DB_USER': 'postgresql',
+            'DB_PASS': 'postgresql'
+        }
+    }
+
+}
