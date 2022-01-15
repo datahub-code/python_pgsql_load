d264f5e64bea6df84c642dc15d125b1966d3e6f2
datahub-code <fahimuddinbd@gmail.com>
2022-01-15 12:52:40 -0500

    UPDATED FILE1

--- dbconfig.py
+++ dbconfig.py
@@ -16,6 +16,22 @@ DB_DETAILS = {
             'DB_USER': 'postgresql',
             'DB_PASS': 'postgresql'
         }
+    },
+    'uat': {
+        'SOURCE_DB': {
+            'DB_TYPE': 'mysql',
+            'DB_HOST': 'postgresql',
+            'DB_NAME': 'postgresql',
+            'DB_USER': 'postgresql',
+            'DB_PASS': 'postgresql'
+        },
+        'TARGET_DB': {
+            'DB_TYPE': 'postgresql',
+            'DB_HOST': 'postgresql',
+            'DB_NAME': 'postgresql',
+            'DB_USER': 'postgresql',
+            'DB_PASS': 'postgresql'
+        }
     }
 
 }
--- main.py
+++ main.py
@@ -2,15 +2,19 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+from dbconfig import DB_DETAILS
 
-
-def main(name):
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    env = sys.argv[1]
+    db_details = DB_DETAILS[env]
+    print(db_details)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('Fahim')
+    main()
 
 
